19801215895952460e9ee2e572ed0a478bc234ad
cherray <navarrocherry17@gmail.com>
2025-04-23 20:31:05 +0800

    Start a game

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,13 @@
-const { defineConfig } = require('@vue/cli-service')
+const { defineConfig } = require('@vue/cli-service');
+const webpack = require('webpack');
+
 module.exports = defineConfig({
-  transpileDependencies: true
-})
+  configureWebpack: {
+    plugins: [
+      new webpack.DefinePlugin({
+        // Properly define the feature flag as a boolean
+        __VUE_PROD_HYDRATION_MISMATCH_DETAILS__: false,
+      }),
+    ],
+  },
+});
